76bd8da34cb78936f9999db028780ae10a7ad8a0
jeffsbaker <coach12@aol.com>
2015-06-22 14:47:56 -0700

    Desktop and Mobile

--- viewport.js
+++ viewport.js
@@ -35,6 +35,7 @@ function updateOrientation()
 		
 	*/
 	var point_width = 0;
+	if (window.cordova) // Only run this command if it is running in a phonegap build app
 	if( /(android)/i.test(navigator.userAgent) )
 	{
 		if (window.orientation == 90 || window.orientation == -90) // landscape
@@ -53,12 +54,17 @@ function updateOrientation()
 	}
 	else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent))
 	{
-		// Fix iphone from zooming when textarea has focus and they change orientation
+		/* Fix iphone from zooming when textarea has focus and they change orientation.
+		 This is no longer needed because of the check_tilt(e) function and using:
+		 "viewport" content="width=534, user-scalable=no"
+		 But if I was using: "viewport" content="user-scalable=no, initial-scale=.6, maximum-scale=.6, minimum-scale=.6"
+		 then I would need to zoom in on the textarea.  However, iOS still zoomed in if
+		 textarea had focus even when using blur() on orientationchange */
 		document.fm.textbox.blur();
 		//window.scrollTo(0,0);
 	//	setTimeout(function(){ 
 	//		var viewport = document.querySelector("meta[name=viewport]"); 
-			if (window.orientation == 90 || window.orientation == -90) // landscape
+	/*		if (window.orientation == 90 || window.orientation == -90) // landscape
 			{
 				if (screen.width <= 320)
 				{
@@ -74,9 +80,50 @@ function updateOrientation()
 					document.fm.textbox.style.zoom = 1;
 				}
 			} 
+		*/
 	//	}, 500);
 	}
 	show_viewport();
+}
+
+function check_tilt(e)
+{
+	
+	var x = Math.round(event.accelerationIncludingGravity.x); // straight= 0, tilt right landscape= 10, tilt left landscape= -10
+    var y = Math.round(event.accelerationIncludingGravity.y); // straight= -10, tilt right landscape= 0, tilt left landscape= 0, on back= 0, on face= 0
+    var z = Math.round(event.accelerationIncludingGravity.z); // straight= 0, on back= -10,  on face= 10
+	var elems = ["input", "textarea"];
+	var blur_flag = 0;
+	
+	if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) // Only run if on iOS device
+	if (window.orientation == 90 || window.orientation == -90) // landscape
+	{
+		if (y <= -7 || y >= 7) // Almost tilting to portrait
+			blur_flag = 1;
+	}
+	else // 0 or 180 (portrait)
+	{
+		if (x >= 7 || x <= -7) // Almost tilting to landscape
+			blur_flag = 1;
+	}
+	
+	if (blur_flag)
+	{
+		for (var i=0; i < elems.length; i++)
+		{
+			var inputs = document.getElementsByTagName(elems[i]);
+			for (var j = 0; j < inputs.length; j++) 
+			{	
+				inputs[j].blur();	
+			}
+		}
+	}
+	
+	if (document.getElementById('footer'))
+	{
+	//	document.getElementById('footer').innerHTML = "x="+x+", y="+y+", z="+z;
+	}	
+
 }
 
 
@@ -107,19 +154,38 @@ function show_viewport()
 			Nexus 7		800x1280		602x889				1.33
 			Nexus 10	2560			1280				2
 			HTC Evo		720x1280		360x640				2
-			iPhone 5	320x568								2	
+			iPhone 5	320x568			534x768				2	
+			iPad-Mini	768x1024		980x1183			1	
 		*/
 	}
 	document.fm.vp.value = viewport.getAttribute("content");
+}
+
+function textbox_focus()
+{
+	document.fm.textbox.scrollTop = document.fm.textbox.scrollHeight;
+
 }
 
 // In Phonegap Build:  event listener deviceready seems to only work with the first function you set it to
 //document.addEventListener('deviceready', show_viewport, false);
 //show_viewport();
 
-window.addEventListener("orientationchange", updateOrientation); // Call when orientation changes
+window.addEventListener("orientationchange", updateOrientation, false); // Call when orientation changes
+window.addEventListener( "devicemotion", check_tilt, false ); // call when phone tilted
 //window.addEventListener("resize", updateOrientation); // Call when orientation changes
 updateOrientation(); // Call on first run of app
 
+//document.fm.textbox.onclick=textbox_focus;
+//window.scrollTo(0,document.body.scrollHeight);
+
+/* Make textbox bigger on desktop browser */
+if (!window.cordova) // Not a phonegap  build app 
+if (!navigator.userAgent.match(/android|ipod|iphone|ipad/i)) // Not a recognized mobile device
+if (window.innerWidth >= 1024) // Not recognizing the viewport width=534
+{
+	// So it must be a desktop.  Zoom in on textarea
+	document.fm.textbox.style.zoom = 1.45;
+}
 
 
